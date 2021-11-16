595671c5d03726a021c1616b3e1c6463415f4287
Johann Barnard <johann.b@telus.net>
2021-11-16 05:57:56 +0000

    Implement basic css-var theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import useLocalStorage from 'use-local-storage';
+import Square from './components/square/Square';
 import './App.css';
 
 function App() {
+  const isDarkModeDefault = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', isDarkModeDefault ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
+      <h1>Easy Darkmode and Themes in React</h1>
+      <button onClick={switchTheme}>
+        Switch to {theme === 'light' ? 'dark' : 'light'} theme
+      </button>
+      <Square />
     </div>
   );
 }
---
+++ src/components/square/Square.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Square.css"
+
+function Square() {
+    return (
+        <div className="square">
+            <span>Look ma, I'm a square!</span>
+        </div>
+    );
+}
+
+export default Square;
